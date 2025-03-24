36415a4bd01fed8b7af0d55e058cd4ed5a8e4f22
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2025-03-24 18:59:24 +0330

    fix: bug
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- pumpkindb_server/src/main.rs
+++ pumpkindb_server/src/main.rs
@@ -6,11 +6,9 @@
 
 #[macro_use]
 extern crate clap;
-#[macro_use]
-extern crate lazy_static;
 extern crate config;
-extern crate pumpkindb_engine;
 extern crate num_cpus;
+extern crate pumpkindb_engine;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
@@ -18,8 +16,10 @@ extern crate mio;
 
 extern crate pumpkindb_mio_server as server;
 
-use pumpkindb_engine::{script, storage, timestamp, lmdb};
+use config::Config;
+use once_cell::sync::Lazy;
 use pumpkindb_engine::script::dispatcher;
+use pumpkindb_engine::{lmdb, script, storage, timestamp};
 
 use clap::{App, Arg};
 
@@ -29,27 +29,31 @@ use std::fs;
 use std::path::PathBuf;
 use std::sync::Arc;
 
-use mio::channel as mio_chan;
+use mio_extras::channel as mio_chan;
 
-
-lazy_static! {
- static ref ENVIRONMENT: lmdb::Environment = {
-    let _ = config::set_default("storage.path", "pumpkin.db");
-    let storage_path = config::get_str("storage.path").unwrap().into_owned();
+static ENVIRONMENT: Lazy<lmdb::Environment> = Lazy::new(|| {
+    let config = Config::builder()
+        .set_default("storage.path", "pumpkin.db")
+        .unwrap()
+        .build()
+        .unwrap();
+    let storage_path = config.get_string("storage.path").unwrap().to_owned();
     fs::create_dir_all(storage_path.as_str()).expect("can't create directory");
-    let map_size = config::get_int("storage.mapsize");
-    let maxreaders = config::get_int("storage.maxreaders").and_then(|v| Some(v as u32));
+    let map_size = config.get_int("storage.mapsize").unwrap();
+    let maxreaders = config
+        .get_int("storage.maxreaders")
+        .map(|v| Some(v as u32))
+        .unwrap();
     if let Some(max) = maxreaders {
-       if max < 1 {
-          error!("storage.maxreaders can't be less than 1");
-          ::std::process::exit(1);
-       }
+        if max < 1 {
+            error!("storage.maxreaders can't be less than 1");
+            ::std::process::exit(1);
+        }
     }
-    storage::create_environment(storage_path, map_size, maxreaders)
- };
-}
+    storage::create_environment(storage_path, Some(map_size), maxreaders)
+});
 
-use pumpkindb_engine::nvmem::{MmapedFile};
+use pumpkindb_engine::nvmem::MmapedFile;
 
 pub fn main() {
     let args = App::new("PumpkinDB Server")
@@ -57,20 +61,30 @@ pub fn main() {
         .about("Event Sourcing Database Engine http://pumpkindb.org")
         .author("PumpkinDB Contributors")
         .setting(clap::AppSettings::ColoredHelp)
-        .arg(Arg::with_name("config")
-            .help("Configuration file")
-            .required(false)
-            .long("config")
-            .short("c")
-            .default_value("pumpkindb.toml")
-            .takes_value(true))
+        .arg(
+            Arg::with_name("config")
+                .help("Configuration file")
+                .required(false)
+                .long("config")
+                .short("c")
+                .default_value("pumpkindb.toml")
+                .takes_value(true),
+        )
         .get_matches();
-    let _ = config::merge(config::Environment::new("pumpkindb"));
-    let _ = config::merge(config::File::new(args.value_of("config").unwrap(),
-                                            config::FileFormat::Toml));
-    let _ = config::set_default("server.port", 9981);
-    let _ = config::set_default("storage.path", "pumpkin.db");
-    let storage_path = config::get_str("storage.path").unwrap().into_owned();
+    let config = Config::builder()
+        .add_source(config::Environment::with_prefix("pumpkindb"))
+        .add_source(config::File::new(
+            args.value_of("config").unwrap(),
+            config::FileFormat::Toml,
+        ))
+        .set_default("server.port", 9981)
+        .unwrap()
+        .set_default("storage.path", "pumpkin.db")
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let storage_path = config.get_string("storage.path").unwrap().to_owned();
     fs::create_dir_all(storage_path.as_str()).expect("can't create directory");
 
     let mut nvmem_pathbuf = PathBuf::from(storage_path);
@@ -80,10 +94,10 @@ pub fn main() {
     let nvmem_hlc = nvmem.claim(20).unwrap();
 
     // Initialize logging
-    let log_config = config::get_str("logging.config");
+    let log_config = config.get_string("logging.config");
     let mut log_configured = false;
-    if log_config.is_some() {
-        let log_file_path = log_config.unwrap().into_owned();
+    if log_config.is_ok() {
+        let log_file_path = log_config.unwrap().to_owned();
         if fs::metadata(&log_file_path).is_ok() {
             log4rs::init_file(&log_file_path, Default::default()).unwrap();
             log_configured = true;
@@ -93,15 +107,19 @@ pub fn main() {
     }
 
     if !log_configured {
-        let appender = log4rs::config::Appender::builder()
-            .build("console",
-                   Box::new(log4rs::append::console::ConsoleAppender::builder().build()));
-        let root =
-            log4rs::config::Root::builder().appender("console").build(log::LogLevelFilter::Info);
-        let _ = log4rs::init_config(log4rs::config::Config::builder()
-            .appender(appender)
-            .build(root)
-            .unwrap());
+        let appender = log4rs::config::Appender::builder().build(
+            "console",
+            Box::new(log4rs::append::console::ConsoleAppender::builder().build()),
+        );
+        let root = log4rs::config::Root::builder()
+            .appender("console")
+            .build(log::LogLevelFilter::Info);
+        let _ = log4rs::init_config(
+            log4rs::config::Config::builder()
+                .appender(appender)
+                .build(root)
+                .unwrap(),
+        );
         warn!("No logging configuration specified, switching to console logging");
     }
 
@@ -122,15 +140,20 @@ pub fn main() {
     for i in 0..cpus {
         debug!("Starting scheduler on core {}.", i);
 
-        let (mut scheduler, sender) =
-            script::Scheduler::new(
-                dispatcher::StandardDispatcher::new(storage.clone(),
-                                                    publisher_accessor.clone(), subscriber_accessor.clone(),
-                                                    timestamp.clone()));
+        let (mut scheduler, sender) = script::Scheduler::new(dispatcher::StandardDispatcher::new(
+            storage.clone(),
+            publisher_accessor.clone(),
+            subscriber_accessor.clone(),
+            timestamp.clone(),
+        ));
         thread::spawn(move || scheduler.run());
         senders.push(sender);
     }
 
-    server::run(config::get_int("server.port").unwrap(),
-                senders, relay_sender, relay_receiver);
+    server::run(
+        config.get_int("server.port").unwrap(),
+        senders,
+        relay_sender,
+        relay_receiver,
+    );
 }
